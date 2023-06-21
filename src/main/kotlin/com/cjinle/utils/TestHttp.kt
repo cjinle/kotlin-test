397bbfa7616b84f9d877a3521787a0cdc4864ea6
cjinle <cjinle@gmail.com>
2023-06-21 17:31:34 +0800

    xx

--- src/main/kotlin/com/cjinle/utils/TestHttp.kt
+++ src/main/kotlin/com/cjinle/utils/TestHttp.kt
@@ -3,9 +3,8 @@ package com.cjinle.utils
 import okhttp3.*
 import java.io.IOException
 
-const val API_URL = "https://www.51voa.com/Technology_Report_1.html"
-
 class TestHttp {
+    private val url = "https://www.51voa.com/Technology_Report_1.html"
     fun run() {
         println("test http start ... ")
         sync()
@@ -13,7 +12,7 @@ class TestHttp {
 
     fun sync() {
         val client = OkHttpClient()
-        var request = Request.Builder().url(API_URL).build()
+        var request = Request.Builder().url(url).build()
 
         val response = client.newCall(request).execute()
         println(response.body?.string())
@@ -21,7 +20,7 @@ class TestHttp {
 
     fun async() {
         val client = OkHttpClient()
-        var request = Request.Builder().url(API_URL).build()
+        var request = Request.Builder().url(url).build()
 
         client.newCall(request).enqueue(object: Callback{
             override fun onFailure(call: Call, e: IOException) {
---
+++ src/main/kotlin/com/cjinle/utils/TestRetrofit2.kt
@@ -0,0 +1,10 @@
+package com.cjinle.utils
+
+class TestRetrofit2 {
+    private val apiUrl = "http://192.168.56.101"
+
+    fun run() {
+
+    }
+}
+
